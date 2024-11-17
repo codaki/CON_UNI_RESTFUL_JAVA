39dbcba691439e7e37e6e5f1380f446ad798b132
Christopher Iza <cdiza5@espe.edu.ec>
2024-11-16 19:37:11 -0500

    Creación de aplicativo móvil

---
+++ 02.CLIMOV/CON_UNI_RESTFUL_JAVA_MOV/app/src/main/java/ec/edu/monster/con_uni_restful_java_mov/MainActivity.java
@@ -0,0 +1,14 @@
+package ec.edu.monster.con_uni_restful_java_mov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ 02.CLIMOV/CON_UNI_RESTFUL_JAVA_MOV/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CON_UNI_RESTFUL_JAVA_MOV"
+include(":app")
+ 
