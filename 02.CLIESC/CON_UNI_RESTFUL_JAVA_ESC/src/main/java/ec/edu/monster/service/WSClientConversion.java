c144582737ddf53917cda67ae114dea1b43bf804
Christopher Iza <cdiza5@espe.edu.ec>
2024-11-17 18:49:05 -0500

    Creación service en el escritorio

---
+++ 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/service/WSClientConversion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/JerseyClient.java to edit this template
+ */
+package ec.edu.monster.service;
+
+/**
+ * Jersey REST client generated for REST resource:ConversionResource
+ * [conversion]<br>
+ * USAGE:
+ * <pre>
+ *        WSClientConversion client = new WSClientConversion();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author danie
+ */
+public class WSClientConversion {
+    
+}
---
+++ 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/service/WSClientLogin.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/JerseyClient.java to edit this template
+ */
+package ec.edu.monster.service;
+
+/**
+ * Jersey REST client generated for REST resource:LoginResource [login]<br>
+ * USAGE:
+ * <pre>
+ *        WSClientLogin client = new WSClientLogin();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author danie
+ */
+public class WSClientLogin {
+    
+}
