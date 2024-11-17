968f744769c1972f8ed0ae65a9fac3619fc2285f
MelShared <mstinoco@espe.edu.ec>
2024-11-17 18:35:50 -0500

    Creación cliente de escritorio

---
+++ 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/CON_UNI_RESTFUL_JAVA_ESC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.monster;
+
+import ec.edu.monster.view.LoginView;
+
+/**
+ *
+ * @author JOSE
+ */
+public class CON_UNI_RESTFUL_JAVA_ESC {
+
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        
+        // Hacer visible la ventana de Login
+        loginView.setVisible(true);
+    }
+      
+}
---
+++ 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/model/ConversionModel.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.monster.model;
+
+/**
+ *
+ * @author JOSE
+ */
+public class ConversionModel {
+    public static class ConversionResponse {
+        private double resultado;
+        private String toUnit;
+        private String fromUnit;
+        private double valor;
+
+        public ConversionResponse(double valorConvertido, String unidadDestino, String unidadInicial, double valorOriginal) {
+            this.resultado = valorConvertido;
+            this.toUnit = unidadDestino;
+            this.valor = valorOriginal;
+            this.fromUnit = unidadInicial;
+        }
+
+        public ConversionResponse() {
+        }
+
+        public double getResultado() {
+            return resultado;
+        }
+
+        public void setResultado(double resultado) {
+            this.resultado = resultado;
+        }
+
+        public String getToUnit() {
+            return toUnit;
+        }
+
+        public void setToUnit(String toUnit) {
+            this.toUnit = toUnit;
+        }
+
+        public String getFromUnit() {
+            return fromUnit;
+        }
+
+        public void setFromUnit(String fromUnit) {
+            this.fromUnit = fromUnit;
+        }
+
+        public double getValor() {
+            return valor;
+        }
+
+        public void setValor(double valor) {
+            this.valor = valor;
+        }
+    }
+    
+       public static class ConversionRequest {
+        private double valor;
+        private String fromUnit;
+        private String toUnit;
+
+        public ConversionRequest() {
+        }
+
+        
+        public ConversionRequest(double valor, String fromUnit, String toUnit) {
+            this.valor = valor;
+            this.fromUnit = fromUnit;
+            this.toUnit = toUnit;
+        }
+        
+        
+        public double getValor() {
+            return valor;
+        }
+
+        public void setValor(double valor) {
+            this.valor = valor;
+        }
+
+        public String getFromUnit() {
+            return fromUnit;
+        }
+
+        public void setFromUnit(String fromUnit) {
+            this.fromUnit = fromUnit;
+        }
+
+        public String getToUnit() {
+            return toUnit;
+        }
+
+        public void setToUnit(String toUnit) {
+            this.toUnit = toUnit;
+        }
+    }
+}
