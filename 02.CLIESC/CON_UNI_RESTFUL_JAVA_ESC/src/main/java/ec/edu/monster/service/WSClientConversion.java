2b38b2784fc574361d14cc1c0e87b7683d75a966
Christopher Iza <cdiza5@espe.edu.ec>
2024-11-17 18:52:28 -0500

    información netbeans aaa

--- 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/service/WSClientConversion.java
+++ 02.CLIESC/CON_UNI_RESTFUL_JAVA_ESC/src/main/java/ec/edu/monster/service/WSClientConversion.java
@@ -4,6 +4,10 @@
  */
 package ec.edu.monster.service;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
 /**
  * Jersey REST client generated for REST resource:ConversionResource
  * [conversion]<br>
@@ -18,5 +22,22 @@ package ec.edu.monster.service;
  * @author danie
  */
 public class WSClientConversion {
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/WS_CONUNI_RESTFULJAVA/webresources";
+
+    public WSClientConversion() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("conversion");
+    }
+
+    public <T> T convertPressure(Object requestEntity, Class<T> responseType) throws ClientErrorException {
+        return webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON), responseType);
+    }
+
+    public void close() {
+        client.close();
+    }
     
 }
